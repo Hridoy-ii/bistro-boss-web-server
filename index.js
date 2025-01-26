0711f1be38b0586d106c9dbfbb03abf96cbb479f
hridoy-ii <nmohammad.hriidoy@gmail.com>
2025-01-27 01:08:01 +0600

    user delete and make admin api done

--- index.js
+++ index.js
@@ -78,6 +78,19 @@ async function run() {
       res.send(result);
     })
 
+    // Patch or update data
+    app.patch('/users/admin/:id', async (req, res) =>{
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)};
+      const updatedDoc ={
+        $set: {
+          role: 'admin'
+        }
+      }
+      const result = await userCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    })
+
     // to delete a item
     app.delete('/carts/:id', async(req, res) =>{
       const id = req.params.id;
